0447fe3f3d0e7b20b13e04118fbac31240880878
william <18758194783@163.com>
2019-11-14 21:15:13 +0800

    新增supports解析

--- foodie-dev-service/src/main/java/com/imooc/service/impl/TestTransServiceImpl.java
+++ foodie-dev-service/src/main/java/com/imooc/service/impl/TestTransServiceImpl.java
@@ -24,14 +24,15 @@ public class TestTransServiceImpl implements TestTransService {
      * REQUIRED:使用当前的事务，如果当前没有事务，则自己新建一个事务。子方法是必须运行在一个事务中的；
      *          如果当前存在事务，则加入这个事务，成为一个整体；
      *          举例：领导没饭吃，我有钱，我会自己买了自己吃；领导有的吃会分给你一起吃
-     * SUPPORTS:
+     * SUPPORTS:如果当前有事务，则执行事务；如果当前没有事务，则不使用事务。
+     *          举例：领导没饭吃，我也没饭吃；领导有饭吃，我也有饭吃
      * MANDATORY:
      * REQUIRES_NEW:
      * NOT_SUPPORTED:
      * NEVER:
      * NESTED:
      */
-    @Transactional(propagation = Propagation.REQUIRED)
+    //@Transactional(propagation = Propagation.REQUIRED)
     @Override
     public void testPropagationTrans() {
         stuService.saveParent();
