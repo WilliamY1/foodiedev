27e6f05ba9fd800dea69505b42a7533e3d51fcc6
wujunyi <junyi.wu@tigermed.net>
2019-11-14 16:37:14 +0800

    新增Stu的逻辑层和接口

--- foodie-dev-api/src/main/java/com/imooc/Application.java
+++ foodie-dev-api/src/main/java/com/imooc/Application.java
@@ -2,6 +2,7 @@ package com.imooc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
 /**
  * Springboot运行类
@@ -10,6 +11,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @create 2019-11-14 9:51
  **/
 @SpringBootApplication
+// 扫描mybatis通用Mapper所在的包
+@MapperScan(basePackages = "com.imooc.mapper")
 public class Application {
 
     public static void main(String[] args) {
