b5ea1d0f128f1515c968c1e991970739a269c63a
wujunyi <junyi.wu@tigermed.net>
2019-11-18 16:18:42 +0800

    # 新增 商品详情页 # 修复 分类查询问题(没有子分类)

---
+++ foodie-dev-api/src/main/java/com/imooc/controller/ItemsController.java
@@ -0,0 +1,61 @@
+package com.imooc.controller;
+
+import com.imooc.enums.YesOrNo;
+import com.imooc.pojo.*;
+import com.imooc.pojo.vo.CategoryVO;
+import com.imooc.pojo.vo.ItemInfoVO;
+import com.imooc.pojo.vo.NewItemsVO;
+import com.imooc.service.CarouseService;
+import com.imooc.service.CategoryService;
+import com.imooc.service.ItemService;
+import com.imooc.utils.IMOOCJSONResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * 商品controller
+ *
+ * @author WuJunyi
+ * @create 2019-11-14 9:52
+ **/
+@Api(value = "商品接口", tags = {"商品信息展示的相关接口"})
+@RestController
+@RequestMapping("items")
+public class ItemsController {
+
+    @Autowired
+    private ItemService itemService;
+
+    @ApiOperation(value = "查询商品详情", notes = "查询商品详情", httpMethod = "GET")
+    @GetMapping("/info/{itemId}")
+    public IMOOCJSONResult info(
+            @ApiParam(name = "itemId", value = "商品id", required = true)
+            @PathVariable String itemId) {
+        // 判空校验
+        if (null == itemId) {
+            return IMOOCJSONResult.errorMsg("商品不存在");
+        }
+        Items item = itemService.queryItemById(itemId);
+        List<ItemsImg> itemsImgList = itemService.queryItemImgList(itemId);
+        List<ItemsSpec> itemsSpecsList = itemService.queryItemSpecList(itemId);
+        ItemsParam itemsParam = itemService.queryItemParam(itemId);
+
+        ItemInfoVO itemInfoVO = new ItemInfoVO();
+        itemInfoVO.setItem(item);
+        itemInfoVO.setItemImgList(itemsImgList);
+        itemInfoVO.setItemSpecList(itemsSpecsList);
+        itemInfoVO.setItemParams(itemsParam);
+
+        return IMOOCJSONResult.ok(itemInfoVO);
+    }
+
+
+}
